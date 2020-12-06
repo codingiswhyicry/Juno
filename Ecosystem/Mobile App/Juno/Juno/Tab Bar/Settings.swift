aa76e3ebe46fabdb54736bdc249b1bee3abe0fee
Astra <astra@Amandas-MacBook-Pro-2.local>
2020-12-05 19:06:14 -0800

    adding branding changes and working on Juno iOS

---
+++ Ecosystem/Mobile App/Juno/Juno/Tab Bar/Settings.swift
@@ -0,0 +1,30 @@
+//
+//  Settings.swift
+//  Juno
+//
+//  Created by Astra on 12/5/20.
+//  Copyright © 2020 AS. All rights reserved.
+//
+
+import UIKit
+
+class Settings: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ecosystem/Mobile App/Juno/Juno/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Juno
+//
+//  Created by Astra on 11/6/20.
+//  Copyright © 2020 AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
